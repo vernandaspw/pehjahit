0044dd209978da12fa6023cd8a701bae793dc6be
vernandaspw <vernandaspw@gmail.com>
2022-08-30 20:08:38 +0700

    dd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Pehjahit</title>
-    \
+
     <link rel="shortcut icon" href="{{ asset('images/logo-app.png') }}" type="image/x-icon">
 
     <link rel="shortcut icon" href="{{ asset('images/logo-app.png') }}" type="image/x-icon">
